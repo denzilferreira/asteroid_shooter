c1724ba407d09006d2598809116970252bf1c95b
Denzil Ferreira <denzil.ferreira@solita.fi>
2025-06-26 19:31:55 +0300

    Add build.sh script to be used by flatpak cicd

---
+++ build.sh
@@ -0,0 +1,14 @@
+ #!/bin/bash
+install -Dm755 app.sh  /app/bin/asteroid-shooter
+install -Dm644 spaceshooter.py  /app/spaceshooter.py
+install -Dm644 asteroid.png  /app/asteroid.png
+install -Dm644 player1.png  /app/player1.png
+install -Dm644 player2.png  /app/player2.png
+install -Dm644 laser.wav  /app/laser.wav
+install -Dm644 explosion.wav  /app/explosion.wav
+install -Dm644 powerup.wav  /app/powerup.wav
+install -Dm644 space_shooter_loop.wav  /app/space_shooter_loop.wav
+install -Dm644 requirements.txt  /app/requirements.txt
+install -D io.github.denzilferreira.asteroid_shooter.png  /app/share/icons/hicolor/512x512/apps/io.github.denzilferreira.asteroid_shooter.png
+install -D io.github.denzilferreira.asteroid_shooter.desktop  /app/share/applications/io.github.denzilferreira.asteroid_shooter.desktop
+install -D io.github.denzilferreira.asteroid_shooter.metainfo.xml  /app/share/metainfo/io.github.denzilferreira.asteroid_shooter.metainfo.xml
