e93c465bd893d1b816c77a9d52ceadd3f3a903f1
Denzil Ferreira <ferreiradenzil@gmail.com>
2025-06-27 17:48:00 +0300

    Review fixes

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ install -Dm644 laser.wav  /app/laser.wav
 install -Dm644 explosion.wav  /app/explosion.wav
 install -Dm644 powerup.wav  /app/powerup.wav
 install -Dm644 space_shooter_loop.wav  /app/space_shooter_loop.wav
-install -Dm644 requirements.txt  /app/requirements.txt
 install -Dm644 io.github.denzilferreira.asteroid_shooter.png  /app/share/icons/hicolor/512x512/apps/io.github.denzilferreira.asteroid_shooter.png
 install -Dm644 io.github.denzilferreira.asteroid_shooter.desktop  /app/share/applications/io.github.denzilferreira.asteroid_shooter.desktop
 install -Dm644 io.github.denzilferreira.asteroid_shooter.metainfo.xml  /app/share/metainfo/io.github.denzilferreira.asteroid_shooter.metainfo.xml
